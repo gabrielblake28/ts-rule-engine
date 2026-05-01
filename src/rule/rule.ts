c5f0a31a84fcbf79aba69ed5995155c9287aa577
Gabriel Blake <61571088+gabrielblake28@users.noreply.github.com>
2026-05-01 00:27:56 -0700

    removed description and priority from rule type (priority determined by rules order in the engine input array)

--- src/rule/rule.ts
+++ src/rule/rule.ts
@@ -4,8 +4,6 @@ import { Condition } from "../condition/condition";
 
 export interface Rule<TFacts> {
   name: string,
-  description: string | undefined;
   condition: Condition<TFacts>;
-  priority: number
 }
 
